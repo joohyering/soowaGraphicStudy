5efaa6a060038349fea7adcb78fa2f1f5502225a
joohyering <jooh0106@naver.com>
2021-09-02 12:10:49 +0900

    210902

--- 1/app.js
+++ 1/app.js
@@ -14,7 +14,16 @@ const COLORS = [
 class App {
     constructor() {
         this.canvas = document.createElement('canvas');
+        /* document.creatElement()는 문서 안에 괄호 안에 있는 요소를 만든다는 의미를 가진다. span 태그 등 태그를 이용할 수 있나봄,, 신기해 */
         document.body.appendChild(this.canvas);
+        /* 부모 노드에서 자식 노드를 추가하는 메서드
+        append : 노드 객체와 DOMString(text) 사용 가능, 한 번에 여러 개의 자식 요소 설정 가능
+        appendChild : DOMString 사용 불가, 한 번에 하나의 자식 노드만 생성 가능 */
+        
+        /*Javascript에서 사용되는 Html 요소(element)의 생성(CreateElement), 추가(appendChild)*/
+        /* Html에서 자바스크립트를 사용하게 되면 값을 취득하는 것만큼 동적으로 생성하는 것도 많이 하게 된다.
+        이유는 동적인 효과를 나타내기 위해, 사용자의 환경이나 액션(event)에 따라 페이지의 결과를 달리 나타내기 위해 등 다양 */
+        /* html 바디 부분에 직접 쓰지 않아도 자바스크립트를 활용해 프로그램 적으로 필요한 요소들 추가 가능 */
         this.ctx = this.canvas.getContext('2d');
 
         this.pixelRatio = (window.devicePixelRatio > 1) ? 2 : 1;
